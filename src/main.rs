642b350759fce9dc06185d8bdb5f192eed995772
den-taku <dentaku1412@gmail.com>
2022-06-12 00:34:25 +0900

    Use reference

--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,15 @@ trait ProsimitySearchable {
     /// not actually used in algorithm, but needed to prove the correctness.
     /// proximity: Self::Solutions x Self::Solutions -> 2^Universe
     fn proximity(
-        _solution: Self::Solutions,
-        _solution_: Self::Solutions,
+        _solution: &Self::Solutions,
+        _solution_: &Self::Solutions,
     ) -> std::collections::HashSet<usize> {
         std::collections::HashSet::new()
     }
     /// 1. computable in time polynomial in |U|.
     /// 2. For all S, S* in Self::Solutions, there exists S' in neighbors(S) s.t. |proximity(S', S*)| > |proximity(S, S*)|
     /// 3. For any fixed S*, |proximity(S, S*)| is maximized for (and only for) S = S*
-    fn neighbors(&self, solution: Self::Solutions) -> Vec<Self::Solutions>;
+    fn neighbors(&self, solution: &Self::Solutions) -> Vec<Self::Solutions>;
     /// one solution of the problem is to be idintified in time polynomial in |U|.
     fn start(&self) -> Self::Solutions;
     /// enemurate all solutions
@@ -45,17 +45,13 @@ trait ProsimitySearchable {
         solutions.insert(solution.clone());
         if deps % 2 == 0 {
             println!("{solution}");
-            for s in self.neighbors(solution) {
-                if !solutions.contains(&s) {
-                    self.enume(s, solutions, deps + 1)
-                }
+        }
+        for s in self.neighbors(&solution) {
+            if !solutions.contains(&s) {
+                self.enume(s, solutions, deps + 1)
             }
-        } else if deps % 2 != 0 {
-            for s in self.neighbors(solution.clone()) {
-                if !solutions.contains(&s) {
-                    self.enume(s, solutions, deps + 1)
-                }
-            }
+        }
+        if deps % 2 != 0 {
             println!("{solution}");
         }
     }
